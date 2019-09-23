eb81c569dc45e2d2cd54fd8b1573aa3d3a4009c6
Daniel Las <lasdaniel@gmail.com>
2019-09-23 21:00:19 +0200

    Added missing data

---
+++ src/main/scala/net/oda/data/jira/JiraClient.scala
@@ -0,0 +1,53 @@
+package net.oda.data.jira
+
+import java.util.{Base64, Collections, HashMap, Map}
+
+import com.empirica.rest.client.{Headers, RestClient}
+import net.oda.Config
+import net.oda.data.Rest
+import org.json4s.jackson.Serialization
+import org.json4s.{DefaultFormats, FieldSerializer}
+import org.springframework.http.HttpHeaders
+
+object JiraClient {
+  implicit val formats = DefaultFormats + JiraTimestampSerializer +
+    FieldSerializer[ChangeItem](
+      FieldSerializer.renameTo("toStr", "toString"),
+      FieldSerializer.renameFrom("toString", "toStr")
+    )
+
+  val jiraAuthHeader = Config
+    .getProp("jira.user")
+    .map(_ + ":")
+    .flatMap(h => Config.getProp("jira.apiKey").map(i => h + i))
+    .get
+
+  val jiraHeaders: Map[String, java.util.List[String]] = new HashMap();
+
+  jiraHeaders.put(HttpHeaders.AUTHORIZATION, Collections.singletonList("Basic " + Base64.getEncoder.encodeToString(jiraAuthHeader.getBytes)))
+
+  val restClient = RestClient.using(Rest.httpExecutor)
+    .service(Config.getProp("jira.apiUrl").get)
+    .defaultHeaders(Headers.combine(Rest.jsonHeaders, jiraHeaders))
+
+  val expand = "changelog,-schema,-editmeta"
+  val fields = "resolution,summary,reporter,created,resolutiondate,status,priority,project,issuetype"
+
+  private def getIssuesPage(project: String, startAt: Int = 0): List[JiraIssues] = {
+    val body = restClient.resource("/search?jql=%s&expand=%s&fields=%s&startAt=%s&maxResults=100", s"project = $project", expand, fields, startAt.toString)
+      .getDefault
+      .execute
+      .getBody
+
+    val issues = Serialization.read[JiraIssues](body)
+
+    if (issues.total > startAt) {
+      getIssuesPage(project, startAt + issues.maxResults) ::: List(issues)
+    } else {
+      List(issues)
+    }
+  }
+
+  val searchIssues = (project: String) => getIssuesPage(project).flatMap(_.issues)
+
+}
---
+++ src/test/scala/net/oda/data/jira/JiraIssuesDownloadSpec.scala
@@ -0,0 +1,21 @@
+package net.oda.data.jira
+
+import net.oda.{Config, IO}
+import net.oda.json.JsonSer
+import org.json4s.DefaultFormats
+import org.scalatest.FreeSpec
+
+class JiraIssuesDownloadSpec extends FreeSpec {
+
+  implicit val formats = DefaultFormats + JiraTimestampSerializer
+
+  "Jira issues shoul be downloaded and saved" in {
+    val projectKey = "CRYP"
+    val dataLocation = Config.getProp("data.location").getOrElse(() => "./")
+
+    JiraClient.searchIssues
+      .andThen(JsonSer.writeAsString(formats, _))
+      .andThen(IO.saveTextContent(s"${dataLocation}/${projectKey}-jira-issues.json", _: String))
+      .apply(projectKey)
+  }
+}
