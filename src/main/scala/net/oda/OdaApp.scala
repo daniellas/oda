4995e817e10a8e98ace78502b90a7d58d6cc6994
Daniel Łaś <daniel.las@empirica.pl>
2019-10-15 06:55:47 +0200

    building executable jar

--- src/main/scala/net/oda/OdaApp.scala
+++ src/main/scala/net/oda/OdaApp.scala
@@ -2,9 +2,13 @@ package net.oda
 
 import net.oda.rep.cfd.CFDRest
 import net.oda.vertx.VertxServices
+import org.slf4j.LoggerFactory
 
 object OdaApp {
+  val log = LoggerFactory.getLogger("oda");
+
   def main(args: Array[String]): Unit = {
+    log.info("Starting")
     RestApi.init(VertxServices.router)
     CFDRest.init(VertxServices.router)
     VertxServices.httpServer.requestHandler(VertxServices.router.accept).listen
