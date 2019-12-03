fe9c0b7c85979a1ee0bab0c7f38a9395e78183dd
Daniel Las <lasdaniel@gmail.com>
2019-12-03 20:39:02 +0100

    Back with instant values in CFD

--- src/test/scala/net/oda/rep/SingleReportSpec.scala
+++ src/test/scala/net/oda/rep/SingleReportSpec.scala
@@ -4,6 +4,7 @@ import java.sql.Timestamp
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 
+import com.paulgoldbaum.influxdbclient.Parameter.Precision
 import com.typesafe.scalalogging.Logger
 import net.oda.cfd.{CfdInflux, CfdReporter}
 import net.oda.jira.JiraData
@@ -14,6 +15,9 @@ import org.scalatest.FreeSpec
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import net.oda.Spark.session.implicits._
+import net.oda.cfd.CfdInflux.toPointsOfInts
+import net.oda.influx.InfluxDb
+import net.oda.jira.JiraData.location
 import org.apache.spark.ml.feature.Normalizer
 import org.apache.spark.sql.functions._
 
@@ -24,7 +28,7 @@ class SingleReportsSpec extends FreeSpec {
     val projectKey = "CRYP"
     val project = Config.props.jira.projects(projectKey)
 
-    val vectors = JiraData
+    JiraData
       .loadAsWorkItems(project.estimateMapping.get)
       .andThen(
         CfdReporter
@@ -38,35 +42,12 @@ class SingleReportsSpec extends FreeSpec {
             project.finalState,
             project.stateMapping,
             ChronoUnit.WEEKS,
-            CfdReporter.countAggregate,
+            CfdReporter.sumEstimateAggregate,
             _))
-      .apply(JiraData.location(projectKey))
-      .map(r => (
-        r.getAs[Timestamp](CfdReporter.timeCol),
-        Vectors.dense(
-          r.getAs[Long](CfdReporter.wipCol),
-          r.getAs[Long](CfdReporter.ctCol),
-          r.getAs[Double](CfdReporter.thCol)))
-      )
-      .select('_1.as(CfdReporter.timeCol), '_2.as("vectors"))
-
-    val normalized = new Normalizer()
-      .setInputCol("vectors")
-      .setOutputCol("normVectors")
-      .transform(vectors)
-      .drop('vectors)
-      .map(r => (
-        r.getTimestamp(0),
-        r.getAs[DenseVector](1).apply(0),
-        r.getAs[DenseVector](1).apply(1),
-        r.getAs[DenseVector](1).apply(2)))
-      .select(
-        '_1.as(CfdReporter.timeCol),
-        '_2.as(CfdReporter.wipCol),
-        '_3.as(CfdReporter.ctCol),
-        '_4.as(CfdReporter.thCol))
-
-//    CfdInflux.toPointsNormalized("cfd-normalized",normalized,projectKey,"")
+      .andThen(CfdInflux.toPointsOfDecimals("cfd-estimate", _, projectKey, "All stories and bugs", project.entryState, project.finalState, ChronoUnit.WEEKS.name()))
+      .andThen(InfluxDb.db.bulkWrite(_, Precision.MILLISECONDS))
+      .andThen(Await.result(_, 100 minute))
+      .apply(location(projectKey))
   }
 
 }
